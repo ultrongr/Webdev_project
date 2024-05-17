bd3b32e6068bb7fe2877499eb7d87d6e1633187b
ultrongr <80486057+ultrongr@users.noreply.github.com>
2024-05-17 18:46:05 +0300

    Example query implemented

--- model/better-sqlite/record-model-better-sqlite.mjs
+++ model/better-sqlite/record-model-better-sqlite.mjs
@@ -1,1 +1,19 @@
+'use strict';
 
+// Το better-slite3 είναι εντελώς σύγχρονο
+import db from 'better-sqlite3'
+const sql = new db('model/db/database.db', { fileMustExist: true });
+
+export let getAllArtists = () => {
+    const stmt = sql.prepare('SELECT * FROM ARTIST');
+    let artists;
+    try {
+        artists = stmt.all();
+        return artists;
+    } catch (error) {
+        return error;
+    }
+}
+
+console.log(getAllArtists());
+console.log("end of record-model-better-sqlite.mjs");
